7b4fca979ecc4f0169fa83715e59294bf0a19daa
AlexGowans <alexgowans@hotmail.co.uk>
2018-10-23 22:08:48 -0700

    But I thought "Nah, forget it, yo holmes, to Bel-Air!"

--- AnimationLab/MainPage.xaml.cs
+++ AnimationLab/MainPage.xaml.cs
@@ -110,5 +110,9 @@ namespace AnimationLab
                 speedX *= -1;
             }
         }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e) {
+            DispatcherTimerSetup();
+        }
     }
 }
