e810ed1d2501ae771ea410376b7679a6256d71e6
AlexGowans <alexgowans@hotmail.co.uk>
2018-10-23 22:21:22 -0700

    I whistled for a cab and when it came near  //completed, commenting from here on

--- AnimationLab/MainPage.xaml.cs
+++ AnimationLab/MainPage.xaml.cs
@@ -28,9 +28,8 @@ namespace AnimationLab
         DispatcherTimer dispatcherTimer;
         DateTimeOffset startTime;
         DateTimeOffset lastTime;
-        //DateTimeOffset stopTime;
+
         int timesTicked = 1;
-        //int timesToTick = 10;
 
         int positionX = 100;
         int positionY = 100;
@@ -42,33 +41,18 @@ namespace AnimationLab
             dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += dispatcherTimer_Tick;
             dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 2);
-            //IsEnabled defaults to false
-            //TimerLog.Text += "dispatcherTimer.IsEnabled = " + dispatcherTimer.IsEnabled + "\n";
             startTime = DateTimeOffset.Now;
             lastTime = startTime;
-            //TimerLog.Text += "Calling dispatcherTimer.Start()\n";
             dispatcherTimer.Start();
-            //IsEnabled should now be true after calling start
-            //TimerLog.Text += "dispatcherTimer.IsEnabled = " + dispatcherTimer.IsEnabled + "\n";
         }
 
         void dispatcherTimer_Tick(object sender, object e) {
             DateTimeOffset time = DateTimeOffset.Now;
             TimeSpan span = time - lastTime;
             lastTime = time;
-            //Time since last tick should be very very close to Interval
-            //TimerLog.Text += timesTicked + "\t time since last tick: " + span.ToString() + "\n";
+
             timesTicked++;
-            //if (timesTicked > timesToTick)
-            //{
-            //    stopTime = time;
-            //    TimerLog.Text += "Calling dispatcherTimer.Stop()\n";
-            //    dispatcherTimer.Stop();
-            //    //IsEnabled should now be false after calling stop
-            //    TimerLog.Text += "dispatcherTimer.IsEnabled = " + dispatcherTimer.IsEnabled + "\n";
-            //    span = stopTime - startTime;
-            //    TimerLog.Text += "Total Time Start-Stop: " + span.ToString() + "\n";
-            //}
+
 
             var path1 = new Windows.UI.Xaml.Shapes.Path();
             path1.Fill = new SolidColorBrush(Windows.UI.Colors.DarkSalmon);
